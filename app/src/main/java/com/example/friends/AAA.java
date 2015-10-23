bce4c34267fc62c661edd848f4cbde8a676f28af
yuanjize <1224500078@qq.com>
2015-10-23 16:54:11 +0800

    ssss

---
+++ app/src/main/java/com/example/friends/AAA.java
@@ -0,0 +1,11 @@
+package com.example.friends;
+
+/**
+ * Created by yjz on 2015/10/23.
+ */
+public class AAA {
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+}
